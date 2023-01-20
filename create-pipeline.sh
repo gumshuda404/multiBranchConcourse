e94cf3694603f4bd128bb4c861eeadb6a4f38310
gumshuda404 <120103752+gumshuda404@users.noreply.github.com>
2023-01-20 10:26:58 +0530

    Update create-pipeline.sh

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-export NEW_VERSIONS=$(cat branches/branches)
-export OLD_VERSIONS=$(cat branches/removed)
+#Download fly 
+curl 'http://localhost:8080/api/v1/cli?arch=amd64&platform=linux' -o fly && chmod +x ./fly && sudo mv ./fly /usr/local/bin/
+
+git branch | cut -c 3- >curr_branches.txt
+
+Fgrep -v -f present_pipeline_branches.txt curr_branches.txt >no_pipeline_branches.txt
+
+export new_version=$(cat no_pipeline_branches.txt)
 
 fly -t tutorial login -c http://localhost:8080 -u test -p test 
 
-for version in $NEW_VERSIONS; do
+for version in $new_version; do
   sed "s/___BRANCH___/$version/g" demo/.ci/pipeline-demo.tmpl > demo/.ci/pipeline-app.result
   echo "Create pipeline branch $version"
   fly -t tutorial sp -p app-$version -c demo/.ci/pipeline-app.result
   echo "Unpause pipeline branch $version"
   fly -t tutorial up -p app-$version
+  echo “$version” >> present_pipeline_branches.txt
 done
+
+Fgrep -v -f curr_branches.txt present_pipeline_branches.txt >deleted_branches.txt
+
+export old_version=$(cat branches/deleted_branched.txt)
 
 for version in $OLD_VERSIONS; do
   echo "Delete pipeline branch $version"
   fly -t tutorial dp -p app-$version
 done
+
+Fgrep -v -f present_pipeline_branches.txt deleted_branches.txt >present_pipeline_branches.txt
